254612dcf00573971aecebe70dc1fdf7b0e416fc
Sanjay Singh Shahi <shahi.sanjayshahi@gmail.com>
2026-09-04 10:15:19 +0530

    uncommented node env condition

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,13 +16,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 // make ready for deployment
-// if (process.env.NODE_ENV === "production") {
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.get("/{*any}", (_, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-});
-// }
+  app.get("/{*any}", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running at port: ${PORT}`);
