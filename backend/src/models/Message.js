3cd22902769d5bc03c49c01c673457a50a7dec9e
Sanjay Singh Shahi <shahi.sanjayshahi@gmail.com>
2026-09-08 09:58:12 +0530

    message routes added

---
+++ backend/src/models/Message.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    text: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const Message = mongoose.model("Message", messageSchema);
+
+export default Message;
--- backend/src/routes/route.messages.js
+++ backend/src/routes/route.messages.js
@@ -1,9 +1,19 @@
 import express from "express";
+import {
+  getAllContacts,
+  getChatsByUserId,
+  sendMessage,
+  getChatPartners,
+} from "../controllers/message.controller.js";
+import { arcjectProtection } from "../middleware/arcjet.middleware.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
+router.use(arcjectProtection, protectRoute);
 
-router.get("/send", (req, res) => {
-  res.send("Send Message Route");
-});
+router.get("/contacts", getAllContacts);
+router.get("/chats", getChatPartners);
+router.get("/:id", getChatsByUserId);
+router.post("/send/:id", sendMessage);
 
 export default router;
