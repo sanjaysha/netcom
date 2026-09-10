3fcfc9fc6e2802dff28c23539c8dc1fd1cf16215
Sanjay Singh Shahi <shahi.sanjayshahi@gmail.com>
2026-09-10 11:13:52 +0530

    login UI & functionality

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -30,4 +30,32 @@ export const useAuthUser = create((set, get) => ({
       set({ isSigningUp: false });
     }
   },
+  isLoggingIn: false,
+  login: async (data) => {
+    set({ isLoggingIn: true });
+    try {
+      const res = await axiosInstance.post("/auth/login", data);
+      set({ authUser: res.data });
+      toast.success("Logged in Successfully");
+    } catch (error) {
+      toast.error(error.response.data.message);
+      set({ authUser: null });
+    } finally {
+      set({ isLoggingIn: false });
+    }
+  },
+  isLoggingOut: false,
+  logout: () => {
+    set({ isLoggingOut: true });
+    try {
+      const res = axiosInstance.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("Logged out Successfully");
+    } catch (error) {
+      toast.error("Error logging out");
+      console.log("logout error", error);
+    } finally {
+      set({ isLoggingOut: false });
+    }
+  },
 }));
